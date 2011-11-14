055300c19cf0820a2805dc61f2356d340f9385bb
Ivo van der Wijk <github@in.m3r.nl>
2011-11-14 14:35:24 +0100

    initial setup - refactored two bootstrap code into package

---
+++ README.txt
@@ -0,0 +1,3 @@
+Two - it takes two to Django
+
+[bootstrap][http://twitter.github.com/bootstrap] base templates for Two
---
+++ setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import os
+
+version = '1.0'
+
+setup(name='two.bootstrap',
+      version=version,
+      description="bootstrap templates for two",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      # Get more strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+      classifiers=[
+        "Programming Language :: Python",
+        ],
+      keywords='',
+      author='Ivo van der Wijk',
+      author_email='two@in.m3r.nl',
+      url='http://github.com/iivvoo/two.bootstrap',
+      license='BSD',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['two'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          'setuptools',
+          'two.ol'
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
+
