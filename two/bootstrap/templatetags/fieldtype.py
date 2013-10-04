82d92abdf4ebf6a10a669efe10d6688a6c7b2753
Ivo van der Wijk <github@in.m3r.nl>
2013-10-04 14:03:28 +0200

    preserve original class def

--- two/bootstrap/templatetags/fieldtype.py
+++ two/bootstrap/templatetags/fieldtype.py
@@ -13,4 +13,12 @@ def fieldtype(field, expected):
 
 @register.filter(name='addcss')
 def addcss(field, css):
-    return field.as_widget(attrs={"class":css})
+    attrs = {}
+    try:
+        orig_class = dict(field.field.widget.attrs).get('class', "")
+    except AttributeError:
+        orig_class = ""
+    attrs['class'] = "%s %s" % (orig_class, css)
+
+    
+    return field.as_widget(attrs=attrs)
